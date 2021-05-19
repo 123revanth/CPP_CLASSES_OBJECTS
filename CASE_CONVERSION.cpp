73d7ff088d6e1b31f50c5c607d13a5d8d959c775
123revanth <81552258+123revanth@users.noreply.github.com>
2021-05-19 19:11:02 +0530

    CASE_CONVERSION version 1 patch 0 (1.0)

---
+++ CASE_CONVERSION.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "string"
+#include "algorithm"
+
+using namespace std;
+
+int main(){
+    
+    string input;
+    cout<<"enter a word";
+    getline(cin, input);
+    
+    for(int i =0;i<input.size();i++){
+        if(input[i] >= 'a' && input[i] <= 'z')input[i] -= 32;
+        else input[i] += 32;
+    }
+
+    cout<<input;
+
+    return 0;
+}
